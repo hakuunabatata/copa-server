65a2bf9878871521ca9537da8114336dbb96467a
hakuunabatata <emachadomartins1999@gmail.com>
2022-11-02 12:28:54 -0300

    start prisma

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/pools/count", () => {
-    return { count: 1 };
+  fastify.get("/pools/count", async () => {
+    const pools = await prisma.pool.findMany();
+
+    return { count: pools.length };
   });
 
   await fastify.listen({ port: 3333 });
